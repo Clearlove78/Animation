650f85a46ebfc9b94f52c883b73c8899b634c9e9
Xia junwe <jxia27@binghamton.edu>
2021-10-06 10:40:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import android.app.Activity;
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    float curX = 0;
+    float curY = 0;
+    private AdaptiveIconDrawable animDance;
+    float nextX = 0;
+    float nextY = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView imageView = (ImageView)findViewById(R.id.laoyangfly);
+        final Handler handler = new Handler()
+        {
+
+            @Override
+            public void handleMessage(Message msg)
+            {
+                if(msg.what == 0x123)
+                {
+                    if(nextX > 500)
+                    {
+                        curX = nextX = 0;
+                    }
+                    else
+                    {
+                        nextX += 10;
+                    }
+                    nextY = curY + (float)(Math.random() * 10 - 1);
+                    if(nextY > 1500){
+                        nextY = 0;
+                    }
+                    TranslateAnimation anim = new TranslateAnimation(curX,nextX,curY,nextY);
+                    curX = nextX;
+                    curY = nextY;
+                    anim.setDuration(200);
+                    imageView.startAnimation(anim);
+                }
+            }
+        };
+
+        final AnimationDrawable butterfly = (AnimationDrawable) imageView.getBackground();
+        imageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                butterfly.start();
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        handler.sendEmptyMessage(0x123);
+                    }
+                },0,50);
+            }
+        });
+
+    }
+}
+
